0a97352a565102cfb9edc8241aad93217aa618e6
chrisloarryn <chrisloarryn@gmail.com>
2020-07-01 02:53:50 -0400

    Create chatroom.ts

---
+++ ws/chatroom.ts
@@ -0,0 +1,15 @@
+import { WebSocket } from 'https://deno.land/std/ws/mod.ts';
+import { v4 } from 'https://deno.land/std/uuid/mod.ts';
+
+let sockets = new Map<string, WebSocket>();
+
+const chatConnection = async (ws: WebSocket) => {
+  console.log('new web socket connection');
+
+  // add new ws connection to Map
+  const uid = v4.generate();
+  sockets.set(uid, ws);
+  console.log(sockets)
+};
+
+export { chatConnection };
